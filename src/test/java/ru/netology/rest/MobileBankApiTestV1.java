ea3cd467b13c189de4ff507c546e97cff870ad29
lesdead <lesdead@gmail.com>
2022-05-02 01:52:50 +0300

    project created

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+//                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
